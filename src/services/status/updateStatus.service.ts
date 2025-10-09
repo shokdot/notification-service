aa8ede0545493745a142ffe17e65df41dc2c71dc
Henry Aleksanian (Shok.) <henula09106@gmail.com>
2025-10-09 13:50:30 +0400

    refactor: env url

--- src/services/status/updateStatus.service.ts
+++ src/services/status/updateStatus.service.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
+import { USER_SERVICE_URL } from "src/utils/env.js";
 
 const updateStatus = async (token: string, status: "ONLINE" | "OFFLINE") => {
 	try {
-		const response = await axios.patch(`http://localhost:3001/api/v1/users/me/status`,
+		const response = await axios.patch(`${USER_SERVICE_URL}/me/status`,
 			{ status },
 			{ headers: { "Content-Type": "application/json", "authorization": `Bearer ${token}` } });
 
--- src/utils/env.ts
+++ src/utils/env.ts
@@ -3,10 +3,12 @@ import { z } from "zod";
 const envSchema = z.object({
 	PORT: z.coerce.number().default(3002),
 	HOST: z.string().default("0.0.0.0"),
+	USER_SERVICE_URL: z.string()
 });
 
 const env = envSchema.parse(process.env);
 
 export const PORT = env.PORT;
 export const HOST = env.HOST;
+export const USER_SERVICE_URL = env.USER_SERVICE_URL;
 export const SERVICE_NAME = 'NOTIFICATION_SERVICE';
