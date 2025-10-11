0b247f310991f5ce080646fe6b03d4206be96a1d
Henry Aleksanian (Shok.) <henula09106@gmail.com>
2025-10-12 01:18:40 +0400

    feat: add x-service middleware

--- src/routes/notify.routes.ts
+++ src/routes/notify.routes.ts
@@ -7,10 +7,11 @@ import {
 	markAllReadHandler,
 	deleteNotificationHandler
 } from '@controllers/notification/index.js'
+import serviceAuth from '@core/middlewares/serviceAuth.middleware.js'
 
 export default async function notifyRoutes(app: FastifyInstance): Promise<void> {
 	app.get('/', { preHandler: authenticate }, getNotificationsHandler); //done
-	app.post('/', createNotifcationHandler); //done
+	app.post('/', { preHandler: serviceAuth }, createNotifcationHandler); //done
 	app.patch('/:id/read', { preHandler: authenticate }, markReadHandler); //done except param
 	app.patch('/read-all', { preHandler: authenticate }, markAllReadHandler); //done 
 	app.delete('/:id', { preHandler: authenticate }, deleteNotificationHandler); //done except param
