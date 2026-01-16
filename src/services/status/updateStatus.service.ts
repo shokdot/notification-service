10c2cdb821d6693f3af02554a8a4b2e08064ebbc
Henry Aleksanian (Shok.) <henula09106@gmail.com>
2026-01-17 00:07:15 +0400

    fix: internal request url

--- src/services/status/updateStatus.service.ts
+++ src/services/status/updateStatus.service.ts
@@ -3,7 +3,7 @@ import { USER_SERVICE_URL, SERVICE_TOKEN } from "src/utils/env.js";
 
 const updateStatus = async (userId: string, status: "ONLINE" | "OFFLINE" | "IN_GAME") => {
 	try {
-		const response = await axios.patch(`${USER_SERVICE_URL}/${userId}/status`,
+		const response = await axios.patch(`${USER_SERVICE_URL}/internal/${userId}/status`,
 			{ status },
 			{
 				headers: {
