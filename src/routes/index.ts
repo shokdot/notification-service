853d6273104d85627ad18752dca0f2a67a24b4e7
Henry Aleksanian (Shok.) <henula09106@gmail.com>
2026-01-17 00:54:37 +0400

    fix: add route registration

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from "fastify";
 import wsRoutes from "./ws.routes.js";
 import notifyRoutes from "./notify.routes.js";
+import internalRoutes from "./internal.routes.js";
 
 export default async function notificationRoutes(app: FastifyInstance): Promise<void> {
 	app.register(notifyRoutes);
 	app.register(wsRoutes);
+	app.register(internalRoutes, { prefix: "/internal" });
 }
